5413aaa852d974038a278a749b4966c7f1528226
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-09-09 17:15:32 +0530

    chat-app-server

---
+++ index.js
@@ -0,0 +1,27 @@
+//Node server to handel socket io connections
+
+// const io = require('socket.io')(8000)
+const io = require('socket.io')(port, {
+    cors: {
+      origin: '*',
+    }
+  });
+ const port = process.env.PORT || 8000; 
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
